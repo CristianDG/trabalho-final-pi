abd29b1d5a8d76c326f118edc3ef975715b344ae
CristianDG <39601699+CristianDG@users.noreply.github.com>
2021-07-21 08:34:16 -0300

    Add t4

--- t4.py
+++ t4.py
@@ -3,8 +3,32 @@ import matplotlib.pyplot as plt
 
 #T4. O maior vencedor de <Tipo de Medalha> num mesmo evento.
 
-def main():
-    print("Escolha o tipo de medalha:")
-    tipo_medalha = info.criar_menu(['Ouro','Prata','Bronze'], only_index = True)
+def pegar_dados_para_plotagem():
+    medalha = info.escolher_medalha()
+    tipo_olimpiada = info.escolher_tipo_olimpiada()
+    ano = info.escolher_ano(tipo_olimpiada)
 
-    medal = { 3: 'Bronze', 2: 'Silver', 1: 'Gold' }.get(tipo_medalha, 'Bronze')
+    contador_medalhas = {}
+    for nome, medalha_ganha in info.medalhas_partecipantes(ano,tipo_olimpiada):
+        if medalha == medalha_ganha:
+            contador_medalhas[nome] = contador_medalhas.get(nome,0) + 1
+
+    maior_quantidade = max(contador_medalhas.values())
+    maiores_vencedores = [nome for (nome, qtd_medalhas) in contador_medalhas.items() if qtd_medalhas == maior_quantidade]
+    return maiores_vencedores, (maior_quantidade, medalha, ano, tipo_olimpiada)
+
+
+def main():
+    maiores_vencedores, argumentos_titulo = pegar_dados_para_plotagem()
+    qtd, medalha, ano, tipo_olimpiada = argumentos_titulo
+
+    final = 'medalha conquistada' if qtd == 1 else 'medalhas conquistadas'
+
+    if len(maiores_vencedores) == 1:
+        vencedor = maiores_vencedores[0]
+        print('O maior vencedor de medalhas de {} das olimpíadas de {} de {} foi {} com {} {}'.format(medalha, tipo_olimpiada, ano, vencedor, qtd, final))
+    else:
+        vencedores = ', '.join(maiores_vencedores)
+        print('Os maiores vencedores de medalhas de {} das olimpíadas de {} de {} foram {} com {} {}'.format(medalha,tipo_olimpiada, ano, vencedores, qtd, final))
+
+
